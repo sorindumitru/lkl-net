8917828d698597bebb7fdc6cd183fae8fc191905
Sorin Dumitru <dumitru.sorin87@gmail.com>
2010-05-17 20:06:58 +0300

    ?

--- scripts/webdns.sh
+++ scripts/webdns.sh
@@ -7,3 +7,4 @@ sleep 2
 
 ip link set tap0 up
 ip address add 192.168.100.200 dev tap0
+ip route add 192.168.100.0/24 via 192.168.100.200 dev tap0
--- scripts/webroutes.sh
+++ scripts/webroutes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ip route add 192.168.100.0/24 dev tap0
-ip route add 192.168.200.0/24 dev tap0
+ip route add 192.168.100.0/24 via 192.168.100.123 dev tap0
+ip route add 192.168.200.0/24 via 192.168.100.123 dev tap0

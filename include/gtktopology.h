122d80ab8c2813f97f3a54b1ef36408411f96f8f
Sorin Dumitru <dumitru.sorin87@gmail.com>
2010-06-10 11:25:40 +0300

    removed warnings from hypergui

--- include/gtktopology.h
+++ include/gtktopology.h
@@ -1,13 +1,18 @@
 #ifndef GTK_TOPOLOGY_H_
 #define GTK_TOPOLOGY_H_
 
+#include <list.h>
 #include <gtk/gtk.h>
 
 typedef struct _GtkTopology                 GtkTopology;
 typedef struct _GtkTopologyClass            GtkTopologyClass;
+typedef struct _GtkTopologyDevice           GtkTopologyDevice;
+
 
 struct _GtkTopology {
 	GtkDrawingArea parent;
+
+	struct list_head devices;
 };
 
 struct _GtkTopologyClass {
@@ -20,5 +25,7 @@ struct _GtkTopologyClass {
 #define GTK_IS_TOPOLOGY(obj)               (G_TYPE_CHECK_INSTANCE_TYPE((obj), GTK_TYPE_TOPOLOGY))
 #define GTK_IS_TOPOLOGY_CLASS(obj)         (G_TYPE_CHECK_CLASS_TYPE((obj), GTK_TYPE_TOPOLOGY))
 #define GTK_TOPOLOGY_GET_CLASS             (G_TYPE_INSTANCE_GET_CLASS((obj), GTK_TYPE_TOPOLOGY, GtkTopologyClass))
+
+GtkWidget* gtk_topology_new();
 
 #endif /* GTK_TOPOLOGY_H_ */
